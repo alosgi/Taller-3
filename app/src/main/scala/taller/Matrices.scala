320ad8225794a36a0cdd89dab8c06b8bb091762e
alosgi <osorio.alejandra@correounivalle.edu.co>
2024-12-06 01:30:05 -0500

    se creo la funcion prodpunto

--- app/src/main/scala/taller/Matrices.scala
+++ app/src/main/scala/taller/Matrices.scala
@@ -10,4 +10,8 @@ package object Matrices{
         val v = Vector.fill(long,long){random.nextInt(vals)}
     }
 
+    def prodpunto(v1: Vector[Int], v2: Vector[Int]): Int={
+        (v1 zip v2).map({case(1,j)=>(i*j)}).sum
+    }
+
 }
