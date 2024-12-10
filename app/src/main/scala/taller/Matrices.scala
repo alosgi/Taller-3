b0200cfa1c65c9a1327de24205106919133303fd
alosgi <osorio.alejandra@correounivalle.edu.co>
2024-12-10 13:16:29 -0500

    Se creo la funcion mulMatriz

--- app/src/main/scala/taller/Matrices.scala
+++ app/src/main/scala/taller/Matrices.scala
@@ -10,13 +10,19 @@ package object Matrices{
         val v = Vector.fill(long,long){random.nextInt(vals)}
     }
 
-    def prodpunto(v1: Vector[Int], v2: Vector[Int]): Int={
+    def prodPunto(v1: Vector[Int], v2: Vector[Int]): Int={
         (v1 zip v2).map({case(1,j)=>(i*j)}).sum
     }
 
     def transpuesta (m: Matriz): Matriz ={
         val l=m.length
         Vector.tabulate(l,l)((i,j)=>m(j)(i))
+    }
+
+    def mulMatriz(m1: Matriz, m2: Matriz): Matriz = {
+        val m2t = (transpuesta(m2))
+        Vector.tabulate(m1.length, m2.length){
+            case (i,j)=> prodPunto(m1(i), m2t(j))}
     }
 
 }
